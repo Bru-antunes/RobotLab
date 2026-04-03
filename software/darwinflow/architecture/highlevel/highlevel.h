2920e452d797cebf04e3f45e0b5043f6e07176f3
Bruna Antunes <96384921+Bru-antunes@users.noreply.github.com>
2026-04-03 18:29:57 -0300

    Create highlevel.h

---
+++ software/darwinflow/architecture/highlevel/highlevel.h
@@ -0,0 +1,9 @@
+#ifndef HIGH_LEVEL_H
+#define HIGH_LEVEL_H
+
+// ============================
+// Task do High Level
+// ============================
+void task_high_level(void *pvParameters);
+
+#endif
